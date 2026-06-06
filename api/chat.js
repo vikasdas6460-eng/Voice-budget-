99102312adda53957b29142292709dd1b31bbcd4
vikasdas6460-eng <vikasdas6460@gmail.com>
2026-06-06 18:07:47 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,4 @@
+export const config = { api: { bodyParser: true } };
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
